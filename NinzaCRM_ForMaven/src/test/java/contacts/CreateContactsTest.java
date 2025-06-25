99a498e63ca72f79fe7cc0f5e6d672f50474c644
Tanusree Ghosh <Tanusree Ghosh@LAPTOP-S5KOB1D2>
2025-06-25 22:40:36 +0530

    Contacts Changes Tanusree

---
+++ NinzaCRM_ForMaven/src/test/java/contacts/CreateContactsTest.java
@@ -0,0 +1,12 @@
+package contacts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateContactsTest {
+	@Test
+	public void creatContactTest() {
+		Reporter.log("Contacts Created Successfully", true);
+	}
+
+}
