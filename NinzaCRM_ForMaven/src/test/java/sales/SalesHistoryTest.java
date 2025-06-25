63e4c9f02793337f9af77b477cf16c8e4aeea782
Jayshree Chaure <89380314+jayshree-chaure@users.noreply.github.com>
2025-06-25 11:03:52 +0530

    Jayshree: Sales Page changes

---
+++ NinzaCRM_ForMaven/src/test/java/sales/SalesHistoryTest.java
@@ -0,0 +1,13 @@
+package sales;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SalesHistoryTest {
+	
+	@Test(groups = "SmokeTest")
+	public void saleHistoryTest() {
+		Reporter.log("sale HistoryTest ",true);
+	}
+
+}
