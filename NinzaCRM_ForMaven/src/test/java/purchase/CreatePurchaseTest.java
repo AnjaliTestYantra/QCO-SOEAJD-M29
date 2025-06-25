d39ca26465dd9a77f76b3acbc0fc7650648d8991
ammug <ammug@ArjunAmmu>
2025-06-25 11:05:45 +0530

    Purchase order - committed

---
+++ NinzaCRM_ForMaven/src/test/java/purchase/CreatePurchaseTest.java
@@ -0,0 +1,11 @@
+package purchase;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreatePurchaseTest {
+	@Test
+	public void createPurchaseordertest() {
+		Reporter.log("Sales Created ", true);
+	}
+}
