32714562903aea146ace915dab12b3cded9d4f2a
Admin <Admin@DESKTOP-86NSD9J>
2025-06-25 11:03:40 +0530

    Harshitha_Contacts

---
+++ NinzaCRM_ForMaven/src/test/java/contacts/CreateContactsTest.java
@@ -0,0 +1,15 @@
+package contacts;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateContactsTest {
+	
+	@Test
+	public void createcontactTest()
+	{
+		Reporter.log("Created Sales", true);
+	}
+	
+}
