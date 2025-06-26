d6dda84a86602b668d34e3136eee6d4ebb6a9813
dpben <dpben@LAPTOP-DU42LFCB>
2025-06-26 17:10:37 +0530

    lead added

--- NinzaCRM_ForMaven/src/test/java/Lead/LeadTest.java
+++ NinzaCRM_ForMaven/src/test/java/Lead/LeadTest.java
@@ -7,7 +7,7 @@ public class LeadTest {
 	public void addleadTest() {
 		Reporter.log("Clicked add button",true);
 		Reporter.log("updated required details",true);
-		Reporter.log("Lead added successfully",true);
+		Reporter.log("LEAD added successfully",true);
 	}
 	
 }
