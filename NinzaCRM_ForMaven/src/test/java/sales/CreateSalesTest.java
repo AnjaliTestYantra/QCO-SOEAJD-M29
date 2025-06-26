67570807d590351a145a673d969532b3cf5f5f2a
Admin <Admin@AshwiniRajgire>
2025-06-26 10:14:22 +0530

    sales commited: Ashwini

--- NinzaCRM_ForMaven/src/test/java/sales/CreateSalesTest.java
+++ NinzaCRM_ForMaven/src/test/java/sales/CreateSalesTest.java
@@ -1,5 +1,13 @@
 package sales;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class CreateSalesTest {
+	@Test
+	public void createSaleTest()
+	{
+		Reporter.log("SuccessFully Created Sales",true);
+	}
 
 }
